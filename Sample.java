fed165fffedc381988dab15d2100fd68e4d70421
Example Surname <sukina2020@gmail.com>
2014-09-19 15:16:00 +0530

    Sample.java

---
+++ Sample.java
@@ -0,0 +1,8 @@
+import java.io.*;
+public class Sample
+{
+	public static void main(String[] args)throws IOException {
+		System.out.println("hello");
+	}
+}
+
---
+++ sample.java
@@ -0,0 +1,8 @@
+import java.io.*;
+class Sample
+{
+public static void main(string[])throws IOException
+{
+System.out.println"hello";
+}
+}
